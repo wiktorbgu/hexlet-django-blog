0aee8cdb70c0f7dbd9c19e20f431f6f8073b7bd4
wiktorbgu <wiktorbgugmail.com>
2024-01-18 21:34:24 +0300

    commit

--- hexlet_django_blog/urls.py
+++ hexlet_django_blog/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from hexlet_django_blog import views
 
 urlpatterns = [
+    path('', views.index), # <- добавляем эту строчку
+    path('about/', views.about), # <- добавляем эту строчку
     path('admin/', admin.site.urls),
 ]
